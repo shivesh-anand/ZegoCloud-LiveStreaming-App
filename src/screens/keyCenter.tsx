d1148778068be3b780960c8032e09efa6cfd394d
Shivesh Anand <btech10218.21@bitmesra.ac.in>
2024-11-09 11:24:52 +0530

    Update keyCenter.tsx

--- src/screens/keyCenter.tsx
+++ src/screens/keyCenter.tsx
@@ -1,6 +1,6 @@
 const KeyCenter = {
-  appID: 1667733543,
-  appSign: 'fb0e07fa2b37091821728fd4449c10c766e8bedec030ab70cf2d74647985c71b',
+  appID: ,
+  appSign: '',
 };
 
 export default KeyCenter;
